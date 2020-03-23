53978cfe5341c13709e9f7ef03c73ebde9872182
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-03-23 10:14:26 +0900

    Chore

--- src/api.cpp
+++ src/api.cpp
@@ -1,15 +1,15 @@
-#include <cstring>
+include<strsafe.h>
 #include <wchar.h>
 #include <windows.h>
 
 #include "api.h"
 
-void __stdcall DoSomething(Foo *pFoo) {
+    void __stdcall DoSomething(Foo *pFoo) {
   pFoo = reinterpret_cast<Foo *>(CoTaskMemAlloc(sizeof(Foo)));
   pFoo->Name = reinterpret_cast<LPWSTR>(CoTaskMemAlloc(64));
 
   pFoo->Value = 1.23;
-  std::wmemcpy(pFoo->Name, L"Hello, World!", 13);
+  StringCbPrintfW(pFoo->Name, 28, L"Hello, World!");
 
   return;
 }
